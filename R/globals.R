19e27982ac5c3af42be6ef32d29d35df36ac7eec
susanne-207 <dandl.susanne@gmail.com>
2024-01-11 11:06:05 +0100

    resolve check() issues

---
+++ R/globals.R
@@ -0,0 +1,1 @@
+utils::globalVariables(c("feature", "lower", "corrvar", "upper", "res", "importance"))
--- R/summary.R
+++ R/summary.R
@@ -52,8 +52,11 @@ summary.Learner = function(object, resample_result = NULL, control = summary_con
       test_ids = setdiff(dt_row_ids, resample_result$resampling$train_set(i = i))
       assert_true(all(test_ids, resample_result$predictions()[[i]]$data$row_ids))
       test_dt = resample_result$task$clone()$filter(test_ids)$data()
+      models[[i]]$state$train_task = resample_result$task
       if (any(c("pfi", "pdp") %in% control$importance_measures)) {
-        require("iml")
+        if (!requireNamespace("iml", quietly = TRUE)) {
+          stop("Package 'iml' needed for this function to work. Please install it.", call. = FALSE)
+        }
         pred = iml::Predictor$new(model = models[[i]], data = test_dt,
           y = test_dt[, resample_result$task$target_names])
         ## <FIXME:> allow more losses: currently available in IML,
@@ -66,22 +69,23 @@ summary.Learner = function(object, resample_result = NULL, control = summary_con
         if ("pdp" %in% control$importance_measures) {
 
           pdp = iml::FeatureEffects$new(predictor = pred, method = "pdp")
-          imp = lapply(pdp$results, FUN = function(dt) var(dt$.value))
+          imp = lapply(pdp$results, FUN = function(dt) stats::var(dt$.value))
           imp = data.table(feature = names(pdp$results),
             importance = as.vector(unlist(imp), "numeric"))
           imps[["pdp"]] = data.table(rbind(imps[["pdp"]], imp))
         }
       }
+      models[[i]]$state$train_task = NULL
     }
 
     compute_importance_measures = function(dt) {
       mm = dt[, mean(importance), by = feature]
       setnames(mm, "V1", "mean")
-      var = dt[, var(importance), by = feature]
+      vardt = dt[, stats::var(importance), by = feature]
       corr = 1/num_models + length(test_ids)/(length(dt_row_ids) - length(test_ids))
-      var$corrvar = corr * var$V1
-      setnames(var, "V1", "var")
-      merge(mm, var, by = "feature")
+      vardt$corrvar = corr * vardt$V1
+      setnames(vardt, "V1", "var")
+      merge(mm, vardt, by = "feature")
     }
 
     imps_res = lapply(imps, compute_importance_measures)
