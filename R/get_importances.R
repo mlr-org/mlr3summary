5c692c79598673f1e8318efd457ee85360a36b1c
susanne-207 <dandl.susanne@gmail.com>
2024-01-31 10:40:56 +0100

    update importance calculation code acc. to mlr3::score functions

--- R/get_importances.R
+++ R/get_importances.R
@@ -1,48 +1,89 @@
-get_importances = function(resample_result, importance_measures) {
-  imps = c()
-  models = resample_result$learners
-  num_models = length(models)
-  dt_row_ids = resample_result$task$row_ids
-  for (i in 1:num_models) {
-    test_ids = setdiff(dt_row_ids, resample_result$resampling$train_set(i = i))
-    assert_true(all(test_ids, resample_result$predictions()[[i]]$data$row_ids))
-    test_dt = resample_result$task$clone()$filter(test_ids)$data()
-    models[[i]]$state$train_task = resample_result$task
-    if (any(c("pfi", "pdp") %in% importance_measures)) {
-      if (!requireNamespace("iml", quietly = TRUE)) {
-        stop("Package 'iml' needed for this function to work. Please install it.", call. = FALSE)
-      }
-      pred = iml::Predictor$new(model = models[[i]], data = test_dt,
-        y = test_dt[, resample_result$task$target_names])
-      pfiind = grepl("pfi", importance_measures)
-      if (any(pfiind)) {
-        pfis = importance_measures[pfiind]
-        lses = gsub("pfi.", "", pfis)
-        for (l in lses) {
-          imp = iml::FeatureImp$new(predictor = pred, loss = l)$results
-          imps[[paste0("pfi.", l)]] = data.table(rbind(imps[[paste0("pfi.", l)]], imp))
-        }
-      }
-      if ("pdp" %in% importance_measures) {
+## inspired by mlr3:::score_measures and mlr3:::score_single_measure
+get_importances = function(obj, importance_measures) {
 
-        pdp = iml::FeatureEffects$new(predictor = pred, method = "pdp")
-        imp = lapply(pdp$results, FUN = function(dt) stats::var(dt$.value))
-        imp = data.table(feature = names(pdp$results),
-          importance = as.vector(unlist(imp), "numeric"))
-        imps[["pdp"]] = data.table(rbind(imps[["pdp"]], imp))
-      }
-    }
-    models[[i]]$state$train_task = NULL
+  tab = get_private(obj)$.data$as_data_table(view = NULL,
+    reassemble_learners = TRUE, convert_predictions = FALSE)
+  tmp = unique(tab, by = c("task_hash", "learner_hash"))[,
+    c("task", "learner"), with = FALSE]
+  # correction factor acc. Molnar et al. (2023),
+  # https://doi.org/10.1007/978-3-031-44064-9_24  p. 468,
+  # based on Nadeau, C., Bengio, Y.: Inference for the generalization error. Mach. Learn. 52(3),
+  # 239–281 (2003)
+  correction_factor = 1/obj$iters + mean(sapply(1:obj$iters, FUN = function(i) {
+    length(obj$resampling$test_set(i = i)) / length(obj$resampling$train_set(i = i))
+  }))
+
+  imps_list = sapply(importance_measures, function(x) NULL)
+
+  # step through importance measures
+  for (imp_msr in importance_measures) {
+
+    # step through resample folds
+    imps = pmap_dtr(tab[, c("task", "learner", "resampling",
+      "iteration", "prediction"), with = FALSE], function(task,
+        learner, resampling, iteration, prediction) {
+        get_single_importance(imp_msr, task, learner, train_set = resampling$train_set(iteration),
+          prediction)
+      })
+
+    # aggregate results (mean, variance, corrected variance acc. to Molnar et al. (2023), see above)
+    imps_list[[imp_msr]] = aggregate_importance(imps, correction_factor)
   }
 
-  compute_importance_measures = function(dt) {
-    mm = dt[, mean(importance), by = feature]
-    setnames(mm, "V1", "mean")
-    vardt = dt[, stats::var(importance), by = feature]
-    corr = 1/num_models + length(test_ids)/(length(dt_row_ids) - length(test_ids))
-    vardt$corrvar = corr * vardt$V1
-    setnames(vardt, "V1", "var")
-    merge(mm, vardt, by = "feature")
+  imps_list
+}
+
+get_single_importance = function(importance_measure, task, learner, train_set, prediction) {
+
+  test_ids = prediction$test$row_ids
+  test_tsk = task$clone()$filter(test_ids)
+  learner$state$train_task = task
+
+  if (grepl("pfi", importance_measure)) {
+    loss = gsub("pfi.", "", importance_measure)
+    importance_measure = "pfi"
   }
-  lapply(imps, compute_importance_measures)
+
+  switch(importance_measure,
+    "pdp" = get_pdp_importance(learner, test_tsk),
+    "pfi" = get_pfi_importance(learner, test_tsk, loss))
+  # <FIXME:> TODO
+  # "shap" = get_shap_importance(learner, test_tsk))
+
+}
+
+
+get_pdp_importance = function(learner, test_tsk) {
+  if (!requireNamespace("iml", quietly = TRUE)) {
+    stop("Package 'iml' needed for this function to work. Please install it.", call. = FALSE)
+  }
+  pred = iml::Predictor$new(model = learner, data = test_tsk$data(),
+    y = test_tsk$target_names)
+  pdp = iml::FeatureEffects$new(predictor = pred, method = "pdp")
+  imp = lapply(pdp$results, FUN = function(dt) stats::var(dt$.value))
+  data.table(feature = names(pdp$results),
+    importance = as.vector(unlist(imp), "numeric"))
+}
+
+
+get_pfi_importance = function(learner, test_tsk, loss) {
+  if (!requireNamespace("iml", quietly = TRUE)) {
+    stop("Package 'iml' needed for this function to work. Please install it.", call. = FALSE)
+  }
+  pred = iml::Predictor$new(model = learner, data = test_tsk$data(),
+    y = test_tsk$target_names)
+  imp = iml::FeatureImp$new(predictor = pred, loss = loss)$results[, c("feature", "importance")]
+}
+
+get_shap_importance = function(learner, test_tsk, loss) {
+  # <FIXME:> TODO
+}
+
+aggregate_importance = function(dt, correction_factor) {
+  mm = dt[, mean(importance), by = feature]
+  setnames(mm, "V1", "mean")
+  vardt = dt[, stats::var(importance), by = feature]
+  vardt$corrvar = correction_factor * vardt$V1
+  setnames(vardt, "V1", "var")
+  merge(mm, vardt, by = "feature")
 }
